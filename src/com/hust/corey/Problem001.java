7067be25697985a33f4169fc118b05bba07e95d4
Zeng Hui <714676641@qq.com>
2017-08-10 15:56:07 +0800

    update problem001

--- src/com/hust/corey/Problem001.java
+++ src/com/hust/corey/Problem001.java
@@ -12,7 +12,8 @@ import java.util.Scanner;
  * 最高分是多少（华为）
  * url:
  * https://www.nowcoder.com/practice/3897c2bcc87943ed98d8e0b9e18c4666?tpId=49&tqId=29275&tPage=1&rp=1&ru=/ta/2016test&qru=/ta/2016test/question-ranking
- *
+ * extra:
+ * Scanner的平均耗时是BufferedReader的10倍左右
  */
 public class Problem001 implements Problem{
     @Override
@@ -20,7 +21,6 @@ public class Problem001 implements Problem{
         solution();
     }
     void solution(){
-        ArrayList<Integer> result;
         Scanner in = new Scanner(System.in);
         int N,M;
         int[] scores;
@@ -28,7 +28,6 @@ public class Problem001 implements Problem{
             N = in.nextInt();
             M = in.nextInt();
             scores = new int[N];
-            result = new ArrayList<>(M);
             for(int i=0;i<N;i++){
                 scores[i] = in.nextInt();
             }
@@ -37,12 +36,9 @@ public class Problem001 implements Problem{
                 int start = in.nextInt();
                 int end = in.nextInt();
                 if(cmd == 'Q')
-                    result.add(getHighest(scores,start,end));
+                    System.out.println(getHighest(scores,start,end));
                 else
                     scores[start-1] = end;
-            }
-            for(int res : result){
-                System.out.println(res);
             }
         }
     }
@@ -52,15 +48,12 @@ public class Problem001 implements Problem{
             start = end;
             end = tmp;
         }
-        int length = end - start + 1;
-        int[] copy = new int[length];
-        int max;
-        System.arraycopy(scores,start-1,copy,0,length);
-        max = copy[0];
-        for(int num : copy){
-            if(num > max)
-                max = num;
+        int max = scores[start-1];
+        for(int i = start-1;i<end;i++){
+            if(scores[i] > max)
+                max = scores[i];
         }
+
         return max;
     }
 }
